1ff52df3d2b68c3959f48024534ab66a476992c9
Kyle Atchison <katchison@gmail.com>
2017-08-24 17:32:24 -0500

    Merge branch 'master' of https://github.com/TheDude1656/liri-node-app

--- liri.js
+++ liri.js
@@ -1,1 +1,27 @@
 var keys = require("./keys.js").twitterKeys;
+
+// console.log(keys);
+// for (key in keys) {
+//   console.log(key + " = " + keys[key]);
+// }
+var client = new Twitter({
+  consumer_key: keys.consumer_key,
+  consumer_secret: keys.consumer_secret,
+  access_token_key: keys.access_token_key,
+  access_token_secret: keys.access_token_secret
+});
+
+var params = {
+  screen_name: '_DHole_'
+};
+
+// console.log(client);
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    for (tweet in tweets) {
+
+      console.log(tweets[tweet].text);
+    }
+
+  }
+});
