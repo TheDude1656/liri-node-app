07fd9cb05551803541bb90b4edb9b51e1700e3fc
Kyle Atchison <katchison@gmail.com>
2017-08-27 15:02:05 -0500

    homework changes

--- liri.js
+++ liri.js
@@ -1,8 +1,10 @@
 var keys = require("./keys.js");
-
+var request = require("request");
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var myTweets = "";
+var fs = require("fs");
+
 var spotify = new Spotify({
   id: keys.spotifyKeys.client_id,
   secret: keys.spotifyKeys.client_secret
@@ -16,21 +18,26 @@ var client = new Twitter({
 var params = {
   screen_name: '_DHole_'
 };
-var action = process.argv[2]
-switch (action) {
-  case "my-tweets":
-    twitter();
-    break;
-  case "spotify-this-song":
-    spotify();
-    break;
-  case "movie-this":
-    movie();
-    break;
-  case "do-what-it-says":
-    dowhat();
-    break;
+var action = process.argv[2];
+var input = process.argv.splice(3).join(" ");
+
+function work() {
+  switch (action) {
+    case "my-tweets":
+      twitter();
+      break;
+    case "spotify-this-song":
+      spotifySong();
+      break;
+    case "movie-this":
+      movie();
+      break;
+    case "do-what-it-says":
+      dowhat();
+      break;
+  }
 }
+work();
 
 function twitter() {
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -39,23 +46,55 @@ function twitter() {
         myTweets = (tweets[tweet].text);
         console.log(myTweets);
       }
+
     }
   })
 };
 
-function spotify() {
+function spotifySong(action) {
+
   spotify.search({
     type: 'track',
-    query: action
+    query: input
   }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-
     console.log(data);
   });
 };
 
-function movie() {};
+function movie() {
 
-function dowhat() {};
+  var queryUrl = "http://www.omdbapi.com/?t=" + (input || "Mr.Nobody") + "&y=&plot=short&apikey=40e9cece";
+
+  request(queryUrl, function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      console.log("Plot: " + JSON.parse(body).Plot);
+    }
+  });
+};
+
+function dowhat() {
+
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log(data);
+
+    var dataArr = data.split(",");
+    action = dataArr[0];
+    input = dataArr[1];
+    work(action, input);
+  });
+};
